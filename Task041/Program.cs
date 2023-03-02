eda60e7c8078d3a53249bd2ab0dfbbbb7a0ef2c8
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-03-02 23:22:52 +0500

    Task041 - greaterThanZeroElement

---
+++ Task041/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+
+Console.Write("Определение чисел больше 0 -> Введите числа через пробел: ");
+String elements = Console.ReadLine();
+
+int[] array = GetArrayFromString(elements);
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+Console.WriteLine($"Количество чисел больше 0 = {greaterThanZeroElement(array)} ");
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numbers = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numbers.Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result[i] = int.Parse(numbers[i]);
+    }
+    return result;
+}
+
+int greaterThanZeroElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
